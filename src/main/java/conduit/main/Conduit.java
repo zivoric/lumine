91f3055fcb089b5c8d69a57128584612fbc9fc51
zivor <zivor@jty-laptop-21.fios-router.home>
2021-02-11 08:58:55 -0500

    working on getting minecraft injects

--- src/main/java/conduit/main/Conduit.java
+++ src/main/java/conduit/main/Conduit.java
@@ -1,10 +1,17 @@
 package conduit.main;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.minecraft.client.main.Main;
+import net.minecraft.server.command.CommandManager;
 
 public class Conduit {
+	private static final Logger LOGGER = LogManager.getLogger();
+	private static final CommandManager cmdManager = new CommandManager(CommandManager.RegistrationEnvironment.field_25419);
     public static void main(String[] args) {
-    	System.out.println("Hi...");
+    	ZCommand.register(cmdManager.getDispatcher());
+    	LOGGER.info("Conduit main class successfully initialized.");
     	Main.main(args);
     }
 }
