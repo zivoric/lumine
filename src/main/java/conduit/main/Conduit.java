c7ae30bdb4ef66a20618097a42eae5a16e03969d
Jasper T-Y <zivoricok@gmail.com>
2021-02-11 01:38:53 -0500

    initial content

---
+++ src/main/java/conduit/main/Conduit.java
@@ -0,0 +1,10 @@
+package conduit.main;
+
+import net.minecraft.client.main.Main;
+
+public class Conduit {
+    public static void main(String[] args) {
+    	System.out.println("Hi...");
+    	Main.main(args);
+    }
+}
