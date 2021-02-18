ffef79e76ee669c25f927b99248cf88b0cfc6e7e
zivor <zivor@jty-laptop-21.fios-router.home>
2021-02-18 10:54:16 -0500

    added unfinished commandbridge class

--- src/main/java/conduit/netmc/command/Commands.java
+++ src/main/java/conduit/netmc/command/Commands.java
@@ -7,7 +7,7 @@ public class Commands {
 	private static final List<Command> BASE_COMMANDS = new ArrayList<Command>();
 	public static final Command TEST_COMMAND = register(new TestCommand());
 	public static final Command VERSION_COMMAND = register(new VersionCommand());
-	private static Command register(Command command) {
+	public static Command register(Command command) {
 		boolean canPut = true;
 		for (Command c : BASE_COMMANDS) {
 			if (command.getClass().isInstance(c)) {
--- src/main/java/conduit/netmc/command/TestCommand.java
+++ src/main/java/conduit/netmc/command/TestCommand.java
@@ -2,6 +2,7 @@ package conduit.netmc.command;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 
 import conduit.chat.ChatUtils;
 import net.minecraft.server.command.CommandManager;
@@ -11,13 +12,16 @@ import net.minecraft.text.LiteralText;
 public class TestCommand extends Command {
 	private static int egg = 0;
 	public void register(CommandDispatcher<ServerCommandSource> commandDispatcher) {
-		commandDispatcher.register(CommandManager.literal("zcomm").executes(ex -> {
+		commandDispatcher.register(getBuilder());
+	}
+	private LiteralArgumentBuilder<ServerCommandSource> getBuilder() {
+		return CommandManager.literal("ctest").executes(ex -> {
 			ex.getSource().sendFeedback(new LiteralText(ChatUtils.CHAT_PREFIX + "Test number: " + ChatUtils.highlightWord(""+egg) + "."), false);
 			return 0;
 		}).then(CommandManager.argument("value", IntegerArgumentType.integer(0, 255)).executes(ex -> {
 			ex.getSource().sendFeedback(new LiteralText(ChatUtils.CHAT_PREFIX + "Test number set to " + ChatUtils.highlightWord(""+IntegerArgumentType.getInteger(ex, "value"))), false);
 			egg = IntegerArgumentType.getInteger(ex, "value");
 			return 0;
-		})));
+		}));
 	}
 }
