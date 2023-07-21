c5c115dc7a083b735deb0cbb59606095ef0249b8
zivoric <52415929+zivoric@users.noreply.github.com>
2023-07-20 23:05:00 -0400

    changed dependencies from 'implementation' to 'compileOnly'

--- prisma/build.gradle.kts
+++ prisma/build.gradle.kts
@@ -17,14 +17,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.apache.logging.log4j:log4j-api:2.19.0")
-    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.19.0")
-    implementation("org.slf4j:slf4j-api:2.0.1")
-    implementation("net.sf.jopt-simple:jopt-simple:5.0.4")
-    implementation("org.ow2.asm:asm:9.1")
-    implementation("org.ow2.asm:asm-tree:9.1")
-    implementation("org.ow2.asm:asm-commons:9.1")
-    implementation("com.google.guava:guava:28.0-jre")
+    compileOnly("org.apache.logging.log4j:log4j-api:2.19.0")
+    compileOnly("org.apache.logging.log4j:log4j-slf4j2-impl:2.19.0")
+    compileOnly("org.slf4j:slf4j-api:2.0.1")
+    compileOnly("net.sf.jopt-simple:jopt-simple:5.0.4")
+    compileOnly("org.ow2.asm:asm:9.1")
+    compileOnly("org.ow2.asm:asm-tree:9.1")
+    compileOnly("org.ow2.asm:asm-commons:9.1")
+    compileOnly("com.google.guava:guava:28.0-jre")
 }
 
 version = "1.0.0"
